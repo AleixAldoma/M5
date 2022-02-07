57913d9db7d5e11d7778768434e5c823a633a94f
AleixAldoma <95371451+AleixAldoma@users.noreply.github.com>
2022-02-07 15:29:59 +0100

    Comentaris afegits

--- moda.java
+++ moda.java
@@ -9,9 +9,9 @@ public class Moda {
 	int nLletres1 = 4;
 	int[] num1 = {4,6,8,4};
 	
-	@Test
+	@Test //Comproba si la cantitat de "lÃ­nies" es superior a 0.
 	public void TestModaLinies() {
-		assertFalse("Nº de línies no valid", nLinies <= 0);
+		assertFalse("NÂº de lÃ­nies no valid", nLinies <= 0);
 	}
 	
 	@Test
@@ -19,26 +19,26 @@ public class Moda {
 		System.out.println(nLinies);
 		System.out.println("");
 		System.out.println(nLletres1);
-		assertFalse("Es necessita mínim 1 nombre", nLletres1 <= 0);
+		assertFalse("Es necessita mÃ­nim 1 nombre", nLletres1 <= 0); //En cas de no mirar ningÃºn nombre per a comparar, donar error.
 		
 		int top = 0;
 		int oldC = 0;
 		for(int i=0;i<num1.length;i++) {
-			System.out.println(num1[i]);
+			System.out.println(num1[i]); //Mostrar en pantalla cada nombre.
 			
 			int c = 0;
-			int aux=num1[i];
-			assertFalse(num1[i]+" no es valid", num1[i] < 0 || num1[i] > 999);
+			int aux=num1[i]; //Posar el nombre actual en la variable aux.
+			assertFalse(num1[i]+" no es valid", num1[i] < 0 || num1[i] > 999); //En cas de ser un nombre menor a 0 o superior a 999, donar error.
 			for(int n=1;n<num1.length;n++) {
 				if(num1[i]==num1[n]) {
-					assertEquals(num1[i],num1[n]);
+					assertEquals(num1[i],num1[n]); //Comprova si el nombre es el mateix.
 					c++;
 				}
-				if(oldC==0) oldC = c;
-				else if(c>=oldC) top = aux;
+				if(oldC==0) oldC = c; //Si no hi ha cap nombre anterior, posar la quantitat de vegades que ha sortit en el contador antic.
+				else if(c>=oldC) top = aux; //Si el contador actual es major que el contador antic, actualitzar el resultat.
 			}
 		}
 		System.out.println("");
-		System.out.println("El nombre es: "+top);
+		System.out.println("El nombre es: "+top); //Resultat.
 	}
 }
